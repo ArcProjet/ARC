7b41da7d95ae2f323091c9ec20cc7b55d642dd20
Malvin Chevallier <malvin.chevallier@gmail.com>
2022-10-18 00:23:57 +0200

    modif: changement du comportement des grilles, ajouts nombres dynamique de grille

--- JsonLoader.py
+++ JsonLoader.py
@@ -9,37 +9,52 @@ for i in index:
 
 def openJsonFile():
     f = open('data/training/' + indexTab[randrange(len(indexTab))] + '.json','r')
-    y = json.loads(f.read())
+    data = json.loads(f.read())
 
-    grille1 = y['test'][0]
-    grille2 = y['train'][0]
+    grillestrain = []
+    grillestest = []
+    grillestest.append(data['test'][0]['input'])
+    grillestest.append(data['test'][0]['output'])
 
-    return (grille1,grille2)
+    for i in range(0,len(data['train'][0])):
+        grillestrain.append(data['train'][i])
 
-def draw(grille1,grille2):
-    grille1Input = grille1['input']
-    grille1Output = grille1['output']
-    grille2Input = grille2['input']
-    grille2Output = grille2['output']
+    return (grillestrain,grillestest)
 
-    fig = plt.figure(figsize=(5, 5))
-    fig.add_subplot(2, 2, 1)
-    plt.title("Input 1")
+def draw(grillestrain,grillestest):
+    grilleInput = []
+    grilleOutput = []
+
+    for i in range(0,len(grillestrain)):
+        grilleInput.append(grillestrain[i]['input'])
+        grilleOutput.append(grillestrain[i]['output'])
+    
+    row = len(grillestrain)
+    col = 4
+    fig = plt.figure(figsize=(6, 6))
+    cpt = 1
+
+    for i in range(0,row):
+        fig.add_subplot(row, col, cpt)
+        plt.title("Train Input "+str(i))
+        plt.axis('off')
+        plt.imshow(grilleInput[i])
+        cpt = cpt + 1
+        fig.add_subplot(row, col, cpt)
+        plt.title("Train Output "+str(i))
+        plt.axis('off')
+        plt.imshow(grilleOutput[i])
+        cpt = cpt + 1
+    
+    fig.add_subplot(2,2,3)
+    plt.title("Test Input")
     plt.axis('off')
-    plt.imshow(grille1Input)
-    fig.add_subplot(2, 2, 2)
-    plt.title("Output 1")
+    plt.imshow(grillestest[0])
+    fig.add_subplot(2,2,4)
+    plt.title("Test Output")
     plt.axis('off')
-    plt.imshow(grille1Output)
-    fig.add_subplot(2, 2, 3)
-    plt.title("Input 2")
-    plt.axis('off')
-    plt.imshow(grille2Input)
-    fig.add_subplot(2, 2, 4)
-    plt.title("Output 2")
-    plt.axis('off')
-    plt.imshow(grille2Output)
+    plt.imshow(grillestest[1])
     plt.show()
 
-(grille1,grille2) = openJsonFile()
-draw(grille1,grille2)
+grillestrain,grillestest = openJsonFile()
+draw(grillestrain,grillestest)
