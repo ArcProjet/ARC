b88b8a20b11bde8eb286c4a13bb83f360d964dc7
Antoine Marchal <antoine.marchal.dombratgmail.com>
2023-04-03 14:44:36 +0200

    Modification de l'évolution de la population, désormais le brassage est plus aléatoire

--- classePopulation.py
+++ classePopulation.py
@@ -5,11 +5,13 @@ Created on Sat Nov 19 10:57:28 2022
 @author: jeanh
 """
 from classeIndividu import *
-
+import random
 
 taille = 100
 nbCyclesMAX = 100
-
+#taille du groupe de la fonction generernouvelIndividu
+#on prend le meilleur individu d'un groupe de taille_groupe
+taille_groupe=5
 class Population:
     
     def __init__(self,im,dep):
@@ -40,51 +42,63 @@ class Population:
         print("best individu :",self.individus[0].score)
         #self.individus = list(dicoIndividusTemp.keys())
     
+    def genererNouveauIndividu(self):
+        # on sélectionne 5 individus random
+        nombres_random = random.sample(range(taille), taille_groupe)
+        #on choisit le plus petit nombre (correspond au meilleur individu car la liste est triée)
+        meilleur_indiv_groupe1 = min(nombres_random)
 
-    # TODO : Refaire fonction
+        #On recommence avec un deuxième groupe
+        nombres_random = random.sample(range(taille), taille_groupe)
+
+        meilleur_indiv_groupe2 = min(nombres_random)
+
+        #On fait un croisement de ces deux individus
+        #on choisit quelle section intervertir
+        numero_section = random.sample(range(taille), 2)
+        #on les trie pour faciliter la suite
+        nombres_random.sort()
+
+        #On extrait les tableaux de fonctiosn de chaque individus
+        fun_indiv1 = self.individus[meilleur_indiv_groupe1].fonctions
+        fun_indiv2 = self.individus[meilleur_indiv_groupe2].fonctions
+
+        #
+        index_section = slice(numero_section[0], numero_section[1])
+
+        #On copie une section de chaque individus
+        section_indiv1 = fun_indiv1[index_section]
+        section_indiv2 = fun_indiv2[index_section]
+
+        #On échange les sections
+        fun_indiv1[index_section]=section_indiv2
+        fun_indiv2[index_section]=section_indiv1
+
+        #On créé un nouvel individu avec une des deux sections
+        #/!\/!\/!\A l'avenir il faudra créé deux individus d'un coup pour gagner du temps
+        nouvel_individu = Individu(1,self.imageDepart,self.imageEsperee)
+        nouvel_individu.fonctions = fun_indiv1
+        return nouvel_individu
+
+
     def evoluerPopulation1Fois(self): # y a t il une fonction qui permet de trier une liste à partir d'un attribut des éléments de cette même liste
-        
-        # on suppose que la population de départ est triée
-        
-        # 1 On conserve les 50 meilleurs individus pour générer la population suivante
-        # 2 les 10 premiers sont simplement mutés (1 fois)
-        # 3 40 suivants sont créer par croisements entre les 50 premiers
-        # 4 les 50 restants sont générés aléatoirement
+
+
         populationTemp = []
         self.trierPopulation()
-        #for k in range(len(self.individus)):
-        #    print(self.individus[k].score)
-        # commentaire 2
-        for i in range (0, 10):
-            self.individus[i].muter()
-            populationTemp.append(self.individus[i])
-        # commentaire 3
-        for i in range(10, 20):
-            self.individus[i].croiser2Individus(self.individus[0])
-            populationTemp.append(self.individus[i])
-        for i in range(20, 30):
-            self.individus[i].croiser2Individus(self.individus[1])
-            populationTemp.append(self.individus[i])
-        for i in range(30, 40):
-            self.individus[i].croiser2Individus(self.individus[2])
-            populationTemp.append(self.individus[i])
-        for i in range(40, 50):
-            self.individus[i].croiser2Individus(self.individus[3])
-            populationTemp.append(self.individus[i])
-        # commentaire 4
-        for i in range(50, 90):
-            populationTemp.append(Individu(1,self.imageDepart,self.imageEsperee))
-        cpt = 0
-        for i in range(90,100):
-            populationTemp.append(self.individus[cpt])
-            cpt = cpt + 1
+        # On conserve le meilleur individu
+        populationTemp.append(self.individus[0]);
+        for i in range (1,taille):
+            populationTemp.append(self.genererNouveauIndividu())
         self.individus = populationTemp
         self.modifierGrille()
+        return
+
         
     def evolutionPopulation(self):
         nbCycles = 0
         self.modifierGrille()
-        while(self.individus[0].score != 100 and nbCycles < nbCyclesMAX):        
+        while(self.individus[0].score != 100 and nbCycles < nbCyclesMAX):
             self.evoluerPopulation1Fois()
             nbCycles += 1
         self.trierPopulation()
